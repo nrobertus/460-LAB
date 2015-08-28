bfd644b3c9b0d110de3cd9aaf57797363d945c1f
Nathan Robertus <nathan.robertus@gmail.com>
2015-08-28 09:38:13 -0600

    Added a jobs text file and data-massaging to the processor class

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 class processor:
 
     def __init__(self, cores):
+        self.jobs = []
+        f = open("jobs.txt", 'r')
+        jobList = f.read()
+        jobList = jobList.translate(None, '\n').translate(None, ' ').split(';')
+
+
+        for job in jobList:
+            job = job.split(',')
+            if(len(job) == 3):
+                newJob = {'id':job[0], 'arrival':job[1], 'time':job[2]}
+                self.jobs.append(newJob)
+                
+        for job in self.jobs:
+            print job
         self.cores = [];
         for x in range(0,cores):
             c = core()
             self.cores.append(c)
-        for core_ in self.cores:
-            core_.execute_task(10)
-
-
 
 
 class core:
